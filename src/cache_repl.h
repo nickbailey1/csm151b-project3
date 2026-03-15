bb1980a40c67fd84fb2407074ec887b8c480a5d1
nickbailey1 <nbf150@me.com>
2026-03-15 12:16:12 -0700

    implement FIFO replacemenet policy

--- src/cache_repl.h
+++ src/cache_repl.h
@@ -73,27 +73,59 @@ private:
 #define MyReplPolicy LRUReplPolicy
 // Comment the above line out if you would like to attempt extra credits
 // TODO: Implement your own replacement policy
-// class MyReplPolicy : public ReplPolicy {
-// public:
-// 	MyReplPolicy() {}
-// 	~MyReplPolicy() {}
 
-// 	void reset(uint32_t set_count, uint32_t num_ways) override {
-// 		// TODO:
-// 	}
-
-// 	uint32_t pick_victim(uint32_t set, const std::vector<bool>& valid_ways) const override {
-// 		// TODO:
-// 	}
-
-// 	void on_access(uint32_t set, uint32_t way) override {
-// 		// TODO:
-// 	}
-
-// 	void on_fill(uint32_t set, uint32_t way) override {
-// 		// TODO:
-// 	}
-// };
+// FIFO replacement policy.
+// On a miss we evict the way that was filled earliest (round-robin per set).
+// Hits do not change the eviction order
+class MyReplPolicy : public ReplPolicy {
+public:
+	MyReplPolicy() {}
+	~MyReplPolicy() {}
+ 
+	void reset(uint32_t set_count, uint32_t num_ways) override {
+		assert(set_count > 0);
+		assert(num_ways > 0);
+		num_ways_ = num_ways;
+		fill_order_.assign(set_count, std::vector<uint64_t>(num_ways, 0));
+		hand_.assign(set_count, 0);
+		fill_counter_ = 1;
+	}
+ 
+	uint32_t pick_victim(uint32_t set, const std::vector<bool>& valid_ways) const override {
+		// prefer invalid (cold) way first
+		for (uint32_t w = 0; w < valid_ways.size(); ++w) {
+			if (!valid_ways[w]) {
+				return w;
+			}
+		}
+		//otherwise evict FIFO head for this set
+		return hand_[set];
+	}
+ 
+	// hits dont update FIFO order
+	void on_access(uint32_t /*set*/, uint32_t /*way*/) override {}
+ 
+	void on_fill(uint32_t set, uint32_t way) override {
+		fill_order_[set][way] = fill_counter_++;
+		// advance FIFO hand to the oldest slot
+		// find the way with smallest fill timestamp
+		uint32_t oldest_way = 0;
+		uint64_t oldest_ts = fill_order_[set][0];
+		for (uint32_t w = 1; w < num_ways_; ++w) {
+			if (fill_order_[set][w] < oldest_ts) {
+				oldest_ts = fill_order_[set][w];
+				oldest_way = w;
+			}
+		}
+		hand_[set] = oldest_way;
+	}
+ 
+private:
+	uint32_t num_ways_;
+	std::vector<std::vector<uint64_t>> fill_order_;
+	std::vector<uint32_t> hand_;
+	uint64_t fill_counter_;
+};
 
 
 } // namespace tinyrv
